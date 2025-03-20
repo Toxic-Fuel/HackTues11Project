23538e36474c89e75580f044fe1daddc3124da9d
StefchoK <ianakiev209@gmail.com>
2025-03-20 14:04:37 +0200

    Added a wallet script

---
+++ Assets/Scripts/WALLETSCRIPT.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WALLETSCRIPT : MonoBehaviour
+{
+    public class Wallet
+    {
+        private float money = 0;
+
+        public Wallet(float initialMoney = 0)
+        {
+            money = initialMoney;
+        }
+
+        public void AddMoney(float amount)
+        {
+            money += amount;
+            Debug.Log("Robot received " + amount + ". New balance: " + money);
+        }
+
+        public bool RemoveMoney(float amount)
+        {
+            if (money >= amount)
+            {
+                money -= amount;
+                Debug.Log("Robot spent " + amount + ". Remaining balance: " + money);
+                return true;
+            }
+            else
+            {
+                Debug.Log("Not enough money!");
+                return false;
+            }
+        }
+
+        public float GetBalance()
+        {
+            return money;
+        }
+    }
+
+    public Wallet robotWallet;
+
+    void Start()
+    {
+
+    }
+
+    void GiveDailyMoney()
+    {
+
+    }
+}
